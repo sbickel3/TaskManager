369b22d8ace4fde7610e0ec1e33fb56fbbbbc667
sbickel3 <sbickel201@gmail.com>
2026-06-11 21:58:32 -0400

    implementing signal to indicate that navbar should open/close

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,13 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
+isMenuOpen = signal(false);
 
+  toggleMenu() {
+    this.isMenuOpen.update(state => !state);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
 }
